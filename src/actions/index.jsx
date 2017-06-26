16ff35ab6c7ba3c67e35165e6d6c1aa59f4949cf
Vadim Derman <thenoodly@gmail.com>
2017-06-26 00:04:48 -0700

    pagination

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -4,7 +4,7 @@ export const fetchPokeNamesList = (initialItemsPerPage) => {
   return (dispatch, getState) => {
     pokeApi.fetchData().then((response) => {
       let pokeNamesList = response.data.objects[0].pokemon
-      pokeNamesList.map((pokemon, idx) => {
+      return pokeNamesList.map((pokemon, idx) => {
         if (idx > initialItemsPerPage - 1) { return false }
         pokeApi.request(pokemon.resource_uri)
         .then((description) => {
@@ -14,49 +14,51 @@ export const fetchPokeNamesList = (initialItemsPerPage) => {
           pokeNamesList[idx].def = reqData.sp_def
           pokeNamesList[idx].speed = reqData.speed
           pokeNamesList[idx].pkdx_id = reqData.pkdx_id
-          dispatch(setPokeList(pokeNamesList))
+          if(idx === initialItemsPerPage - 1) {
+            dispatch(setPokeList(pokeNamesList))
+          }
         })
       })
     })
   }
 }
 
-// export const fetchPokeDescription = () => {
-//   return (dispatch, getState) => {
-//     pokeApi.fetchData().then((response) => {
-//       const pokeList = response.data.objects[0].pokemon
-//       dispatch(setPokemonList(pokeList))
-//       pokeList.map((pokemon, idx) => {
-//         if (idx >= 20) { return false }
-//         let pokeDescr = {
-//           name: pokemon.name
-//         }
-//         pokeApi.request(pokemon.resource_uri)
-//         .then((description) => {
-//           const reqData = description.data
-//
-//           pokeDescr.type = reqData.types[0].name
-//           pokeDescr.atk = reqData.sp_atk
-//           pokeDescr.def = reqData.sp_def
-//           pokeDescr.speed = reqData.speed
-//
-//           return reqData.sprites[0].resource_uri
-//         })
-//         .then((spriteUrl) => {
-//           pokeApi.request(spriteUrl).then((sprite) => {
-//             pokeDescr.img = sprite.data.image
-//             pokeData.push(pokeDescr)
-//             console.log(pokeData)
-//           })
-//         })
-//       })
-//     })
-//   }
-// }
+export const startUpdatePokeList = (page, sizePerPage) => {
+  return (dispatch, getState) => {
+    const minNumber = (page * sizePerPage) - sizePerPage
+    const maxNumber = page * sizePerPage
+    const pokeNamesList = getState().pokeList
+
+    return pokeNamesList.map((pokemon, idx) => {
+      if (idx > minNumber - 1 && idx < maxNumber) {
+        if (!pokeNamesList[idx].pkdx_id) {
+          pokeApi.request(pokemon.resource_uri)
+          .then((description) => {
+            const reqData = description.data
+            pokeNamesList[idx].type = reqData.types[0].name
+            pokeNamesList[idx].atk = reqData.sp_atk
+            pokeNamesList[idx].def = reqData.sp_def
+            pokeNamesList[idx].speed = reqData.speed
+            pokeNamesList[idx].pkdx_id = reqData.pkdx_id
+
+              dispatch(updatePokeList(pokeNamesList))
+          })
+        }
+      }
+    })
+  }
+}
 
 export const setPokeList = (list) => {
   return {
     type: 'SET_INITIAL_POKE_LIST',
+    list
+  }
+}
+
+export const updatePokeList = (list) => {
+  return {
+    type: 'UPDATE_POKE_LIST',
     list
   }
 }
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -2,6 +2,8 @@ export const pokemonListReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_INITIAL_POKE_LIST':
       return [...action.list]
+    case 'UPDATE_POKE_LIST':
+      return [...action.list]
     default:
       return state
   }
